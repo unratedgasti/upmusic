60969c6889a07258bee9e5017361b06c7a1ff08c
unratedgasti <danieltinao@gmail.com>
2017-12-13 15:00:59 +0800

    front and env
    env with db name and creds

--- public/includes/front/userjs/search.js
+++ public/includes/front/userjs/search.js
@@ -1,16 +1,16 @@
-$('#search_code_button').on('click',function(e){
+/*$('#search_code_button').on('click',function(e){
 	e.preventDefault();
-	var code = $('#search_code');
+	var author = $('#search_author');	
 	var errormessage = $('#errorcode');
 	$('#errorcode').html('');
-	if(code.val() == ""){
+	if(author.val() == ""){
 		$('.alert-danger').removeClass('hidden');
 		errormessage.html('Code is required.');
 	}else{
 		$.ajax({
 			type:"POST",
 			url:$('#methodroute').val(),
-			data:{code:code.val(),'_token':$('meta[name="csrf-token"]').attr('content')},
+			data:{author:author.val(),'_token':$('meta[name="csrf-token"]').attr('content')},
 			beforeSend: function(){
 
 			},
@@ -21,4 +21,45 @@ $('#search_code_button').on('click',function(e){
 		});
 	}
 
+});
+
+*/
+
+
+$('#search_author_ad').on('change',function(){
+		
+		alert($(this).val());
+	if($(this).val()){
+	}else{
+
+				$.ajax({
+			type:"POST",
+			url:$('#methodroute').val(),
+			data:{author:$(this).val(),'_token':$('meta[name="csrf-token"]').attr('content')},
+			beforeSend: function(){
+
+			},
+			success:function(data){
+				alert(data);
+			}
+
+		});
+	}
+})
+
+
+$('#advance').on('click',function(e){
+	e.preventDefault();
+	$('#searchnorm').fadeOut('slow');
+	$('#searchnorm').addClass('hidden');
+	$('#searchad').fadeIn('slow');
+	$('#searchad').removeClass('hidden');
+});
+
+$('#normal').on('click',function(e){
+	e.preventDefault();
+	$('#searchad').fadeOut('slow');
+	$('#searchad').addClass('hidden');
+	$('#searchnorm').fadeIn('slow');
+	$('#searchnorm').removeClass('hidden');
 });
